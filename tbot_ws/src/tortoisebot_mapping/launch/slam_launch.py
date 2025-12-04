1cc76a725cae71e01f66a547bf009294ac4549fb
expozedrookie <hemandsunny@gmail.com>
2025-12-04 09:42:29 +0530

    updated code till 03/12/2025

--- tbot_ws/src/tortoisebot_mapping/launch/slam_launch.py
+++ tbot_ws/src/tortoisebot_mapping/launch/slam_launch.py
@@ -1,24 +1,34 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription,EmitEvent ,LogInfo,TimerAction
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution,FileContent,AndSubstitution,NotSubstitution
+from launch.actions import IncludeLaunchDescription #,EmitEvent ,LogInfo,TimerAction
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution,AndSubstitution,NotSubstitution #,  FileContent
 from launch_ros.substitutions import FindPackageShare
-
-from launch_ros.actions import Node
-from launch.conditions import UnlessCondition,IfCondition
+from launch.actions import DeclareLaunchArgument
+# from launch_ros.actions import Node
+from launch.conditions import IfCondition #,  UnlessCondition
 
 
 
 
 def generate_launch_description():
+    DeclareLaunchArgument('localize',default_value='false', description='true for localization')
+    DeclareLaunchArgument('sync_mapping',default_value='false', description='true for online sync mapping')
+    DeclareLaunchArgument('urdf_file',description='name of the urdf file')
+    DeclareLaunchArgument('urdf_path',description='path of the urdf file')
+    DeclareLaunchArgument('world_file',description='name of the world file')
+    DeclareLaunchArgument('world_path',description='path of the world file')
+    DeclareLaunchArgument('rviz_file',description='name of the rviz config file')
+    DeclareLaunchArgument('rviz_file_path',description='path of the rviz config file')
+
+
     # argument for only enabling localization
-    localize=LaunchConfiguration('localize', default='false')
+    localize=LaunchConfiguration('localize')
     # argument for enabling sync mapping in slam_toolbox
-    sync_mapping=LaunchConfiguration('sync_mapping', default='false')
+    sync_mapping=LaunchConfiguration('sync_mapping')
     # argument for choosing urdf file (name)
     urdf_file=LaunchConfiguration('urdf_file', default='tortoisebot_Task4.urdf')
     # argument for choosing urdf file (path)
     urdf_path=LaunchConfiguration('urdf_path', default=PathJoinSubstitution([FindPackageShare('tortoisebot_description'),'models', urdf_file]))
-    # argument for slam parameter file for any process -async mapping, sync mapping or localization - following three arguments
+    # argument for slam parameter file for any process - async mapping, sync mapping or localization - following three :
     sync_params_file=LaunchConfiguration('slam_params_file', default=PathJoinSubstitution([FindPackageShare('tortoisebot_mapping').find('tortoisebot_mapping'),'config', 'sync_map_config.yaml']))
     localize_params_file=LaunchConfiguration('slam_params_file', default=PathJoinSubstitution([FindPackageShare('tortoisebot_mapping').find('tortoisebot_mapping'),'config', 'localize_config.yaml']))
     async_params_file=LaunchConfiguration('slam_params_file', default=PathJoinSubstitution([FindPackageShare('tortoisebot_mapping').find('tortoisebot_mapping'),'config', 'async_map_config.yaml']))
@@ -34,20 +44,67 @@ def generate_launch_description():
     rviz_config=LaunchConfiguration('rviz_file_path', default=PathJoinSubstitution([FindPackageShare('tortoisebot_description'),'config', rviz_file]))
     
     #Start Gazebo + Rviz Launch file
-    gazebo_rviz_launch_file=IncludeLaunchDescription( PathJoinSubstitution([FindPackageShare('tortoisebot_gazebo'),'launch','gazebo_launch.py']),launch_arguments={'urdf_path':urdf_path,'world_path':world_path,'rviz_file_path':rviz_config,'bridge_param_file':bridge_param_file}.items())
+    gazebo_rviz_launch_file=IncludeLaunchDescription( 
+        PathJoinSubstitution(
+            [FindPackageShare('tortoisebot_gazebo'),'launch','gazebo_launch.py']
+            ),
+            launch_arguments={
+                'urdf_path':urdf_path,
+                'world_path':world_path,
+                'rviz_file_path':rviz_config,
+                'bridge_param_file':bridge_param_file
+            }.items()
+    )
     
     # RUN IF LOCALIZE IS TRUE    
-    slam_localization_launch = IncludeLaunchDescription( PathJoinSubstitution([FindPackageShare('slam_toolbox'),'launch','localization_launch.py']),launch_arguments={'slam_params_file':localize_params_file,'use_sim_time':'true'}.items(),condition=IfCondition(localize))
+    slam_localization_launch = IncludeLaunchDescription(
+        PathJoinSubstitution(
+            [FindPackageShare('slam_toolbox'),'launch','localization_launch.py']
+            ),
+            launch_arguments={
+                'slam_params_file':localize_params_file,
+                'use_sim_time':'true'
+            }.items(),
+            condition=IfCondition(localize)
+    )
     
     
     # RUN IF SYNC MAPPING AND LOCALIZE IS FALSE
-    online_async_slam_mapping_launch = IncludeLaunchDescription( PathJoinSubstitution([FindPackageShare('slam_toolbox'),'launch','online_async_launch.py']),launch_arguments={'slam_params_file':async_params_file,'use_sim_time':'true'}.items(),condition=IfCondition(AndSubstitution(NotSubstitution(sync_mapping),NotSubstitution(localize))))
+    online_async_slam_mapping_launch = IncludeLaunchDescription(
+        PathJoinSubstitution(
+            [FindPackageShare('slam_toolbox'),'launch','online_async_launch.py']
+            ),
+            launch_arguments={
+                'slam_params_file':async_params_file,
+                'use_sim_time':'true'
+            }.items(),
+            condition=IfCondition(AndSubstitution(NotSubstitution(sync_mapping),NotSubstitution(localize)))
+    )
     
     # RUN IF SYNC MAPPING IS TRUE AND LOCALIZE IS FALSE
-    online_sync_slam_mapping_launch = IncludeLaunchDescription( PathJoinSubstitution([FindPackageShare('slam_toolbox'),'launch','online_sync_launch.py']),launch_arguments={'slam_params_file':sync_params_file,'use_sim_time':'true'}.items(),condition=IfCondition(AndSubstitution(sync_mapping,NotSubstitution(localize))))
+    online_sync_slam_mapping_launch = IncludeLaunchDescription(
+        PathJoinSubstitution(
+            [FindPackageShare('slam_toolbox'),'launch','online_sync_launch.py']
+            ),
+            launch_arguments={
+                'slam_params_file':sync_params_file,
+                'use_sim_time':'true'
+            }.items(),
+            condition=IfCondition(AndSubstitution(sync_mapping,NotSubstitution(localize)))
+    )
 
     
     return LaunchDescription([
+        
+        DeclareLaunchArgument('localize',default_value='false', description='true for localization'),
+        DeclareLaunchArgument('sync_mapping',default_value='false', description='true for online sync mapping'),
+        # DeclareLaunchArgument('urdf_file',description='name of the urdf file'),
+        # DeclareLaunchArgument('urdf_path',description='path of the urdf file'),
+        # DeclareLaunchArgument('world_file',description='name of the world file'),
+        # DeclareLaunchArgument('world_path',description='path of the world file'),
+        # DeclareLaunchArgument('rviz_file',description='name of the rviz config file'),
+        # DeclareLaunchArgument('rviz_file_path',description='path of the rviz config file'),
+        # DeclareLaunchArgument('slam_params_file',description='path of the slam_parameter file'),
         
         gazebo_rviz_launch_file,
         slam_localization_launch,
